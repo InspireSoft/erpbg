4bfe5f3e15adb26ca094c63cf679b9c1089acf2f
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-04 16:48:45 +0200

    Change to Item(s) doctype

---
+++ erpbg/public/js/QuotationItem.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Simeon on 4-Dec-17.
+ */
+frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
+    locals[cdt][cdn].divan_modification_image = "/";
+});
