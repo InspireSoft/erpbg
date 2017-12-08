66056a5037e620df94e66023eb591cf4214bf44b
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-08 15:01:02 +0200

    bug fix

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -2,7 +2,7 @@
  * Created by Simeon on 4-Dec-17.
  */
 frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
-    console.log("/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png");
-    cur_frm.set_df_property('website_image', 'options', "/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png");
+    console.log("http://test.aes-systems.de:8003/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png");
+    cur_frm.set_df_property('website_image', 'options', "http://test.aes-systems.de:8003/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png");
     refresh_field('divan_modification_image');
 });
