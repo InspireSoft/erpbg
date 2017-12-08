367fed465a8fb7eae24ab576975df4b3a09059ca
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-08 14:36:19 +0200

    Image in Items

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -2,5 +2,5 @@
  * Created by Simeon on 4-Dec-17.
  */
 frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
-    locals[cdt][cdn].divan_modification_image = "/";
+    locals[cdt][cdn].divan_modification_image = "/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png";
 });
