5a4a1026e1116d0dc9f89a905625167ebfd3d8b5
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-08 16:52:29 +0200

    try fix bug

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -4,14 +4,14 @@
 frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
     if(locals[cdt][cdn].divan_modification=="") {
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-image","");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","#fafbfc");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","#fafbfc");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","140px");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","140px");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","");
         if(!jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon")) {
             jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").addClass("octicon");
         }
@@ -21,13 +21,13 @@ frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cd
      } else {
         console.log("url('/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png') no-repeat center center fixed");
 
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","no-repeat");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","cover");
-        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-image","url('/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png') no-repeat center center fixed");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","cover");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","cover");
-//        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","cover");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","no-repeat");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","cover");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-image","url(/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png) no-repeat center center fixed");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","cover");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","cover");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","cover");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","auto");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","auto");
         if(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon")) {
