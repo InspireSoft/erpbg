2a610144026656bdcf82de156c3f134032a5df0f
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-08 16:36:12 +0200

    try to fix bug

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -2,29 +2,25 @@
  * Created by Simeon on 4-Dec-17.
  */
 frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
-    if(!locals[cdt][cdn].divan_modification) {
+    if(locals[cdt][cdn].divan_modification=="") {
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","140px");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","140px");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-image","");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","#fafbfc");
      } else {
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-image","url('/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png') no-repeat center center fixed");
-    //    jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","400px");
-    //    jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","400px");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","auto");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","auto");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-repeat","no-repeat");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-size","cover");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-webkit-background-size","cover");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-moz-background-size","cover");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("-o-background-size","cover");
-    //    background-image: url("/private/files/divan_01.png");
-    //    width: 400px;
-    //    height: 400px;
-    //    background-repeat: no-repeat;
-    //  -webkit-background-size: cover;
-    //  -moz-background-size: cover;
-    //  -o-background-size: cover;
-    //  background-size: cover;
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
     }
 });
