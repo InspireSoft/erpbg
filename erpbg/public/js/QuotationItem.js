7e0fe02160b58a30304540055de5c5eef30208f5
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-08 18:37:26 +0200

    change to onload

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -1,8 +1,9 @@
 /**
  * Created by Simeon on 4-Dec-17.
  */
-function load_image(frm, cdt, cdn) {
-    if(locals[cdt][cdn].divan_modification=="") {
+function load_image(item) {
+    console.log("load_image");
+    if(item.divan_modification=="") {
 
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").each(function() {
             if (jQuery(this).find('img').length) {
@@ -20,7 +21,7 @@ function load_image(frm, cdt, cdn) {
             jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").addClass("octicon-circle-slash");
         }
      } else {
-        console.log("url('/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png') no-repeat center center fixed");
+        console.log("url('/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png') no-repeat center center fixed");
 
         var hasI = false;
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").each(function() {
@@ -29,9 +30,9 @@ function load_image(frm, cdt, cdn) {
             }
         });
         if(!hasI) {
-            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html()+"<img src=\"/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png\" />");
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html()+"<img src=\"/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png\" />");
         } else {
-            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image img").attr("src",function() {return "/private/files/divan_"+(locals[cdt][cdn].divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png";});
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image img").attr("src",function() {return "/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png";});
         }
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
         jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","auto");
@@ -47,15 +48,23 @@ function load_image(frm, cdt, cdn) {
 
 
 frappe.ui.form.on("Quotation", "onload_post_render", function (frm, cdt, cdn) {
-    if(frm.doc.items != null && frm.doc.items.length > 0) {
-        frm.doc.items.forEach(function(item) {
-            load_image(frm, "Quotation Item", item.name);
+    console.log("onload");
+    jQuery("div[data-fieldname='items'] span.octicon-triangle-down").click(function() {
+        var a = jQuery(this).closest("div[data-idx]");
+        console.log(a);
+        console.log(a.attr("data-name"));
+        cur_frm.doc.items.forEach(function(item) {
+            if(item.name == a.attr("data-name")) {
+                window.setTimeout(function() {
+                    load_image(item);
+                }, 500);
+            }
         });
-    }
+    });
 });
 
 
 frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
-    load_image(frm, cdt, cdn);
+    load_image(locals[cdt][cdn]);
 });
 
