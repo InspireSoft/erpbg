0c5a19acbc14d45f1e663595a9a6c4f675077cb4
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-11 13:18:19 +0200

    fixed an extra dash in cnumber in Quotation

--- erpbg/erpbg/quotation/__init__.py
+++ erpbg/erpbg/quotation/__init__.py
@@ -7,7 +7,7 @@ def generate_custom_number(customer):
     number = ""
     quotations = frappe.db.get_values("Quotation", {"customer": ("==", customer)}, "*")
     if len(quotations) > 0:
-        number += str(len(quotations)).zfill(2) + str(datetime.datetime.now().strftime("%m")) + "-"
+        number += str(len(quotations)).zfill(2) + str(datetime.datetime.now().strftime("%m"))
     customer = frappe.db.get_values("Customer", customer, "*")[0]
     if customer.customer_commision > 0:
         if len(number) > 0:
