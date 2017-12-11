5f8194055303a59de9309b0e0773996b08651f22
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-11 12:52:58 +0200

    bug fix

--- erpbg/erpbg/quotation/__init__.py
+++ erpbg/erpbg/quotation/__init__.py
@@ -1,4 +1,5 @@
 import frappe
+import datetime
 
 
 @frappe.whitelist()
@@ -6,14 +7,14 @@ def generate_custom_number(customer):
     number = ""
     quotations = frappe.db.get_values("Quotation", {"customer": ("==", customer)}, "*")
     if len(quotations) > 0:
-        number += str(len(quotations)).zfill(2) + "-"
+        number += str(len(quotations)).zfill(2) + str(datetime.datetime.now().strftime("%m")) + "-"
     customer = frappe.db.get_values("Customer", customer, "*")[0]
     if customer.customer_commision > 0:
         if len(number) > 0:
             number += "-"
-        number += "01"+customer.customer_commision
+        number += "01" + str(customer.customer_commision)
     if customer.customer_cnumber:
         if len(number) > 0:
             number += "-"
-        number += customer.customer_cnumber
+        number += str(customer.customer_cnumber)
     return number
