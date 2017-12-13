33df396ad0bf42f9109922b51140166cbcaf2964
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-13 16:33:45 +0200

    Sales invoice js fix

--- erpbg/public/js/SalesInvoice.js
+++ erpbg/public/js/SalesInvoice.js
@@ -3,6 +3,9 @@
  */
 frappe.ui.form.on("Sales Invoice", "onload_post_render", function (frm, cdt, cdn) {
     if(locals[cdt][cdn].letter_head != "Dimela-Logo-Head") {
-        locals[cdt][cdn].letter_head = "Dimela-Logo-Head";
+        cur_frm.set_value("letter_head", "Dimela-Info-Head");
+    }
+    if(frm.doc.customer) {
+        cur_frm.set_value("customer", frm.doc.customer);
     }
 });
