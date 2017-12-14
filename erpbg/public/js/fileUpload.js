96842e831fb81e7cf6e204c206739977623f5da4
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-14 14:31:05 +0200

    fix for error when uploading

---
+++ erpbg/public/js/fileUpload.js
@@ -0,0 +1,19 @@
+
+console.log(frappe.socketio.SocketIOUploader);
+frappe.socketio.SocketIOUploader.prototype.keep_alive = function() {
+    if (this.next_check) {
+        clearTimeout (this.next_check);
+    }
+    this.next_check = setTimeout (() => {
+        if (!this.started) {
+            // upload never started, so try fallback
+            if (this.fallback) {
+                this.fallback();
+            } else {
+                this.disconnect();
+            }
+        }
+        this.disconnect(false);
+    }, 3000);
+}
+console.log(frappe.socketio.SocketIOUploader);
