1a374e91111a0fde2a8400a995a99312db603ca8
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-18 16:42:02 +0200

    bug fix

--- erpbg/erpbg/production_order/__init__.py
+++ erpbg/erpbg/production_order/__init__.py
@@ -3,5 +3,5 @@ import frappe
 
 @frappe.whitelist()
 def get_sales_order_item(item_name, sales_order):
-    soi = frappe.db.sql("""SELECT * FROM `tabSales Order Item` WHERE `item_name`=%s AND `parent`=%s""", (item_name, sales_order))
+    soi = frappe.db.sql("""SELECT * FROM `tabSales Order Item` WHERE `item_name`=%s AND `parent`=%s""", (item_name, sales_order), as_dict=True)
     return soi[0]
