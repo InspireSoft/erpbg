64ea1a85935b3abd6b860972fdd7df46020bd6ca
Simeon-Banov <simeon.banov@bulwork.com>
2017-12-19 12:25:50 +0200

    js bug fix

--- erpbg/public/js/MaterialRequest.js
+++ erpbg/public/js/MaterialRequest.js
@@ -5,11 +5,15 @@ frappe.ui.form.on("Material Request", "onload_post_render", function (frm, cdt, 
     if(frm.doc.letter_head != "Dimela-Logo-Head") {
         cur_frm.set_value("letter_head", "Dimela-Logo-Head");
     }
+});
+
+frappe.ui.form.on("Material Request Item", "onload_post_render", function (frm, cdt, cdn) {
     if(locals[cdt][cdn].divan_modification != "") {
         type_image(locals[cdt][cdn]);
     }
 });
-frappe.ui.form.on("Quotation Item", "divan_modification", function (frm, cdt, cdn) {
+
+frappe.ui.form.on("Material Request Item", "divan_modification", function (frm, cdt, cdn) {
     if(locals[cdt][cdn].divan_modification != "") {
         type_image(locals[cdt][cdn]);
     }
