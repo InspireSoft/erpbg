1c4d0f72ae24d3bda3797303137315a79eb28849
Simeon-Banov <simeon.banov@bulwork.com>
2018-01-09 12:09:12 +0200

    Revert some changes to find the reason for bugs

--- erpbg/public/js/Communication.js
+++ erpbg/public/js/Communication.js
@@ -2,13 +2,13 @@ frappe.ui.form.on("Communication", {
 	refresh: (frm) => {
 		// setup custom Make button only if Communication is Email
 		if(frm.doc.communication_medium == "Email" && frm.doc.sent_or_received == "Received") {
-            let confirm_msg = "Are you sure you want to create {0} from this email";
-            frm.add_custom_button(__("Quotation"), () => {
-                frappe.confirm(__(confirm_msg, [__("Quotation")]), () => {
-                    frappe.new_doc("Quotation")
-                })
-            }, "Make");
-		    frm.page.set_inner_btn_group_as_primary(__("Make"));
+//            let confirm_msg = "Are you sure you want to create {0} from this email";
+//            frm.add_custom_button(__("Quotation"), () => {
+//                frappe.confirm(__(confirm_msg, [__("Quotation")]), () => {
+//                    frappe.new_doc("Quotation")
+//                })
+//            }, "Make");
+//		    frm.page.set_inner_btn_group_as_primary(__("Make"));
 		}
 	},
 });
