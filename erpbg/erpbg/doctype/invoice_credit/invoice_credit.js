26def9395bbd25de4f4df412019f48f04aa78a56
Simeon-Banov <simeon.banov@bulwork.com>
2018-01-25 08:59:55 +0200

    Invoice Credit and Item js bug fix

--- erpbg/erpbg/doctype/invoice_credit/invoice_credit.js
+++ erpbg/erpbg/doctype/invoice_credit/invoice_credit.js
@@ -533,6 +533,52 @@ var calculate_total_billing_amount =  function(frm) {
 	refresh_field('total_billing_amount')
 }
 
+
+
+function type_image(item) {
+    if(!item.divan_modification || item.divan_modification=="") {
+
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").each(function() {
+            if (jQuery(this).find('img').length) {
+                jQuery(this).remove();
+            }
+        });
+
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","#fafbfc");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","140px");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","140px");
+        if(!jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").addClass("octicon");
+        }
+        if(!jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon-circle-slash")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").addClass("octicon-circle-slash");
+        }
+     } else {
+        //console.log("url('/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_").replace(" ","_"))+".png') no-repeat center center fixed");
+
+        var hasI = false;
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").each(function() {
+            if (jQuery(this).find('img').length) {
+                hasI = true;
+            }
+        });
+        if(!hasI) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html()+"<img src=\"/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_"))+".png\" />");
+        } else {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image img").attr("src",function() {return "/private/files/divan_"+(item.divan_modification.replace("L/20","L20").replace("R/20","R20").replace(" ","_").replace(" ","_"))+".png";});
+        }
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","auto");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","auto");
+        if(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").removeClass("octicon");
+        }
+        if(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon-circle-slash")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").removeClass("octicon-circle-slash");
+        }
+    }
+}
+
 frappe.ui.form.on("Invoice Credit", "onload_post_render", function (frm, cdt, cdn) {
     jQuery("div[data-fieldname='items'] span.octicon-triangle-down").click(function() {
         var a = jQuery(this).closest("div[data-idx]");
