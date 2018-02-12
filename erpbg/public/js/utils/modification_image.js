b92a5758ca79fed291e3dceb4aed932dade7ea3d
Simeon-Banov <simeon.banov@bulwork.com>
2018-02-12 17:46:13 +0200

    bug fix

--- erpbg/public/js/utils/modification_image.js
+++ erpbg/public/js/utils/modification_image.js
@@ -9,14 +9,42 @@ function modification_image(item) {
             },
             callback: function(r) {
                 if(r.message)  {
-                    cur_frm.set_df_property('divan_modification_image', 'options', [r.message.file_url]);
-                    cur_frm.set_df_property("divan_modification_image", "hidden", false);
-                    cur_frm.refresh_field('divan_modification_image');
+                    type_image(r.message.file_url);
                 }
             }
         })
     } else {
-        cur_frm.set_df_property("divan_modification_image", "hidden", true);
-        cur_frm.refresh_field('divan_modification_image');
+        type_image("");
     }
+}
+
+
+function type_image(url) {
+    var df = frappe.meta.get_docfield("items", "divan_modification_image", cur_frm.doc.name);
+    if(url=="") {
+        df.hidden = 1;
+     } else {
+        var hasI = false;
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").each(function() {
+            if (jQuery(this).find('img').length) {
+                hasI = true;
+            }
+        });
+        if(!hasI) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").html()+"<img src=\""+url+"\" />");
+        } else {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image img").attr("src", url);
+        }
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("background-color","white");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("width","auto");
+        jQuery("div[data-fieldname='divan_modification_image'] div.missing-image").css("height","auto");
+        if(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").removeClass("octicon");
+        }
+        if(jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").hasClass("octicon-circle-slash")) {
+            jQuery("div[data-fieldname='divan_modification_image'] div.missing-image i").removeClass("octicon-circle-slash");
+        }
+        df.hidden = 0;
+    }
+    cur_frm.refresh_field('divan_modification_image');
 }
