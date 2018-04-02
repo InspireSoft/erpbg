ed86f67ca46d13f87986af6a6a2267cd27135aaf
Simeon-Banov <simeon.banov@bulwork.com>
2018-04-02 11:23:31 +0300

    Item standard price changes

--- erpbg/erpbg/item/__init__.py
+++ erpbg/erpbg/item/__init__.py
@@ -5,12 +5,21 @@ import frappe
 def update_price_list(iname, iprice):
     item = frappe.db.sql("""SELECT * FROM `tabItem` WHERE `name`=%s""", (iname), as_dict=True)
     item = item[0]
-    price = frappe.new_doc("Item Price")
-    price.currency = "BGN"
-    price.item_name = iname
-    price.selling = 1
-    price.price_list = "Standard Selling"
-    price.item_code = item.item_code
-    price.item_description = item.description
-    price.rate = iprice
-    price.save()
+
+    price_check = frappe.db.sql("""SELECT * FROM `tabItem Price` WHERE `item_name`=%s""", (iname), as_dict=True)
+    if price_check:
+        price_check = price_check[0]
+
+    if price_check:
+        price_check.rate = iprice
+        price_check.save()
+    elif:
+        price = frappe.new_doc("Item Price")
+        price.currency = "BGN"
+        price.item_name = iname
+        price.selling = 1
+        price.price_list = "Standard Selling"
+        price.item_code = item.item_code
+        price.item_description = item.description
+        price.rate = iprice
+        price.save()
