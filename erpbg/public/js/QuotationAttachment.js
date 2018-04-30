e336b47c0460f91cf76e1691665ede937786fbae
Simeon-Banov <simeon.banov@bulwork.com>
2018-04-30 16:15:57 +0300

    fix for Quotation Attachments

--- erpbg/public/js/QuotationAttachment.js
+++ erpbg/public/js/QuotationAttachment.js
@@ -6,3 +6,7 @@ function imageEditor(attachment) {
 frappe.ui.form.on("Quotation Attachment", "attachment", function (frm, cdt, cdn) {
     frm.doc.filename = frm.doc.attachment.replace(/^.*[\\\/]/, '');
 });
+
+frappe.ui.form.on("Quotation", "quotation_attachment_add", function (frm, cdt, cdn) {
+    frm.save();
+});
