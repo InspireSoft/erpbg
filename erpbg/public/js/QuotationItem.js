bcbd92dd2896af961f7f7a800630d25f01bc14d1
Simeon-Banov <simeon.banov@bulwork.com>
2018-05-09 15:52:44 +0300

    Test for Quotation auto Item image adding to Quotation attachments

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -25,30 +25,28 @@ frappe.ui.form.on("Quotation Item", "item_code", function (frm, cdt, cdn) {
             }
         });
     }
-    if(locals[cdt][cdn].image) {
-        frappe.call({
-            method: "frappe.client.get",
-            args: {
-                doctype: "Item",
-                filters: {
-                    item_code: locals[cdt][cdn].item_code
-                }
-            },
-            callback: function(r) {
-                var skip = false;
-                cur_frm.doc.quotation_attachment.forEach(function(attachment) {
-                    if(attachment.name == r.image.name) {
-                        skip = true;
-                    }
-                });
-                if(!skip) {
-                    var child = cur_frm.add_child("quotation_attachment");
-                    frappe.model.set_value(child.doctype, child.name, "attachment", r.image);
-                    cur_frm.refresh();
-                }
+    frappe.call({
+        method: "frappe.client.get",
+        args: {
+            doctype: "Item",
+            filters: {
+                item_code: locals[cdt][cdn].item_code
             }
-        });
-    }
+        },
+        callback: function(r) {
+            var skip = false;
+            cur_frm.doc.quotation_attachment.forEach(function(attachment) {
+                if(attachment.name == r.image.name) {
+                    skip = true;
+                }
+            });
+            if(!skip) {
+                var child = cur_frm.add_child("quotation_attachment");
+                frappe.model.set_value(child.doctype, child.name, "attachment", r.image);
+                cur_frm.refresh();
+            }
+        }
+    });
 });
 
 
