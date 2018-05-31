d2746b89c1d457d7faeb2151e00e8e8c236adb70
Simeon-Banov <simeon.banov@bulwork.com>
2018-05-31 15:40:33 +0300

    remove code to test bug

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -1,11 +1,11 @@
 /**
  * Created by Simeon on 4-Dec-17.
  */
-frappe.ui.form.on("Quotation Item", "divan_modification_link", function (frm, cdt, cdn) {
-    if(locals[cdt][cdn].divan_modification_link != "") {
-        modification_image(locals[cdt][cdn]);
-    }
-});
+// frappe.ui.form.on("Quotation Item", "divan_modification_link", function (frm, cdt, cdn) {
+//     if(locals[cdt][cdn].divan_modification_link != "") {
+//         modification_image(locals[cdt][cdn]);
+//     }
+// });
 
 frappe.ui.form.on("Quotation Item", "item_code", function (frm, cdt, cdn) {
     if(!locals[cdt][cdn].type) {
