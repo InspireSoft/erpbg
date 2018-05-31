fc157e5e771db424c6d1ce8192618c2bcdbe8b53
Simeon-Banov <simeon.banov@bulwork.com>
2018-05-31 13:19:23 +0300

    more bug tests for Quotation

--- erpbg/public/js/Quotation.js
+++ erpbg/public/js/Quotation.js
@@ -99,18 +99,18 @@ frappe.ui.form.on("Quotation", "onload_post_render", function (frm, cdt, cdn) {
     check_for_communication_images(frm);
 
     // set modification_image on Quotation item open window
-    // jQuery("div[data-fieldname='items'] span.octicon-triangle-down").click(function() {
-    //     var a = jQuery(this).closest("div[data-idx]");
-    //     cur_frm.doc.items.forEach(function(item) {
-    //         if(item.name == a.attr("data-name")) {
-    //             if(item.divan_modification_link) {
-    //                 window.setTimeout(function() {
-    //                     modification_image(locals[cdt][cdn]);
-    //                 }, 500);
-    //             }
-    //         }
-    //     });
-    // });
+    jQuery("div[data-fieldname='items'] span.octicon-triangle-down").click(function() {
+        var a = jQuery(this).closest("div[data-idx]");
+        cur_frm.doc.items.forEach(function(item) {
+            if(item.name == a.attr("data-name")) {
+                if(item.divan_modification_link) {
+                    window.setTimeout(function() {
+                        modification_image(locals[cdt][cdn]);
+                    }, 500);
+                }
+            }
+        });
+    });
 
     // jQuery("div[data-fieldname='quotation_attachment'] span.octicon-triangle-down").click(function() {
     //     var a = jQuery(this).closest("div[data-idx]");
--- erpbg/public/js/utils/modification_image.js
+++ erpbg/public/js/utils/modification_image.js
@@ -9,6 +9,8 @@ function modification_image(item) {
             },
             callback: function(r) {
                 if(r.message)  {
+                    console.error("divan_modification file");
+                    console.error(r.message);
                     type_image(r.message.file_url);
                 }
             }
