a5e06a5678a5f3b8a3be68a560d8e9631c017337
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-11 09:23:03 +0300

    fix of bug fix

--- erpbg/public/js/Item.js
+++ erpbg/public/js/Item.js
@@ -60,12 +60,12 @@ frappe.ui.form.on("Item", "onload_post_render", function (frm, cdt, cdn) {
         });
     }
     if(!locals[cdt][cdn].cdescription) {
-        locals[cdt][cdn].cdescription = " ";
+        cur_frm.set_value("cdescription", " ");
     }
     if(!locals[cdt][cdn].note) {
-        locals[cdt][cdn].note = " ";
+        cur_frm.set_value("note", " ");
     }
     if(!locals[cdt][cdn].item_private_calculations) {
-        locals[cdt][cdn].item_private_calculations = " ";
+        cur_frm.set_value("item_private_calculations", " ");
     }
 });
