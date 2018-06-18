e2cee6ea8e3822aef012b8539e66758cd303a673
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-18 15:06:34 +0300

    bug fixes

--- erpbg/erpbg/__init__.py
+++ erpbg/erpbg/__init__.py
@@ -24,22 +24,22 @@ def get_doc_from_print(doctype, docname):
 def item_query(doctype, txt, searchfield, start, page_length, filters, as_dict=False):
 	conditions = []
 	return frappe.db.sql("""select
-            item_name
+            item_name,
+            type,
             if(length(cdescription) > 40,
                 concat(substr(cdescription, 1, 40), "..."), cdescription) as cdecription,
             item_code
-		from tabItem
-		where tabItem.docstatus < 2
-			and tabItem.has_variants=0
-			and tabItem.disabled=0
-			and (tabItem.end_of_life > %(today)s or ifnull(tabItem.end_of_life, '0000-00-00')='0000-00-00')
+		from `tabItem`
+		where docstatus < 2
+			and has_variants=0
+			and disabled=0
+			and (end_of_life > %(today)s or ifnull(end_of_life, '0000-00-00')='0000-00-00')
 			and (item_code LIKE %(txt)s
+			    or type LIKE %(txt)s
 				or item_name LIKE %(txt)s
 				or cdescription LIKE %(txt)s)
 			{fcond} {mcond}
-		order by
-			if(locate(%(_txt)s, item_name), locate(%(_txt)s, item_name), 99999),
-			idx desc, item_name
+		order by item_name
 		limit %(start)s, %(page_len)s """.format(key=searchfield,
 			fcond=get_filters_cond(doctype, filters, conditions).replace('%', '%%'),
 			mcond=get_match_cond(doctype).replace('%', '%%')),
