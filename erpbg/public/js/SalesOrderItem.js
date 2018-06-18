2f2c37d40471f90bbac102889445bc80f9c28bbc
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-18 15:19:00 +0300

    bug fixes

--- erpbg/public/js/SalesOrderItem.js
+++ erpbg/public/js/SalesOrderItem.js
@@ -19,6 +19,14 @@ frappe.ui.form.on("Sales Order Item", "refresh", function (frm, cdt, cdn) {
     locals[cdt][cdn].description = locals[cdt][cdn].cdescription;
 });
 
+
+frappe.ui.form.on("Sales Order Item", "onload_post_render", function (frm, cdt, cdn) {
+
+    cur_frm.fields_dict['items'].grid.get_field("item_code").get_query = function(doc, cdt, cdn) {
+        return {query: "erpbg.erpbg.item_query"}
+    }
+});
+
 frappe.ui.form.on("Sales Order Item", "item_code", function (frm, cdt, cdn) {
     if(!frm.doc.__islocal || frm.doc.__islocal == 0) {
         frappe.call({
