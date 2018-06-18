a8a95e5979e9dd6b69f2ad7757b37b6b87d77fac
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-18 13:05:22 +0300

    addition to geting print format as doc

--- erpbg/erpbg/__init__.py
+++ erpbg/erpbg/__init__.py
@@ -10,6 +10,7 @@ from frappe import _
 @frappe.whitelist()
 def get_doc_from_print(doctype, docname):
     doc = frappe.db.sql("""SELECT * FROM `tab"""+doctype+"""` WHERE `name`=%s""", (docname), as_dict=True)
+    doc = doc[0]
     meta = frappe.get_meta(doc.doctype)
     format = meta.default_print_format
     html = frappe.get_print(doctype, docname, format, doc=doc)
