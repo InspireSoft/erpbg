e17e56885daf1b4a6d22555e7a658d519317a0cf
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-18 13:14:12 +0300

    addition to geting print format as doc

--- erpbg/erpbg/__init__.py
+++ erpbg/erpbg/__init__.py
@@ -11,9 +11,9 @@ from frappe import _
 def get_doc_from_print(doctype, docname):
     doc = frappe.db.sql("""SELECT * FROM `tab"""+doctype+"""` WHERE `name`=%s""", (docname), as_dict=True)
     doc = doc[0]
-    meta = frappe.get_meta(doc.doctype)
-    return meta
-    format = meta.default_print_format
+    # meta = frappe.get_meta(doc.doctype)
+    # format = get_print_format_doc(None, meta)
+    format = "DimelaSalesOrder"
     html = frappe.get_print(doctype, docname, format, doc=doc)
 
     from werkzeug.wrappers import Response
@@ -23,3 +23,20 @@ def get_doc_from_print(doctype, docname):
     response.name = "test.doc"
     response.data = html
     return response
+
+
+
+def get_print_format_doc(print_format_name, meta):
+	"""Returns print format document"""
+	if not print_format_name:
+		print_format_name = frappe.form_dict.format \
+			or meta.default_print_format or "Standard"
+
+	if print_format_name == "Standard":
+		return None
+	else:
+		try:
+			return frappe.get_doc("Print Format", print_format_name)
+		except frappe.DoesNotExistError:
+			# if old name, return standard!
+			return None
