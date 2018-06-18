866305f575616b8cb472e7804eca5a4bd6a0f040
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-18 14:08:22 +0300

    addition to geting print format as doc

--- erpbg/erpbg/__init__.py
+++ erpbg/erpbg/__init__.py
@@ -8,18 +8,21 @@ from werkzeug.wrappers import Response
 
 @frappe.whitelist()
 def get_doc_from_print(doctype, docname):
-    doc = frappe.db.sql("""SELECT * FROM `tab"""+doctype+"""` WHERE `name`=%s""", (docname), as_dict=True)
-    doc = doc[0]
-    # meta = frappe.get_meta(doc.doctype)
-    # format = get_print_format_doc(None, meta)
-    format = "DimelaSalesOrder"
+    try:
+        doc = frappe.db.sql("""SELECT * FROM `tab"""+doctype+"""` WHERE `name`=%s""", (docname), as_dict=True)
+        doc = doc[0]
+        # meta = frappe.get_meta(doc.doctype)
+        # format = get_print_format_doc(None, meta)
+        format = "DimelaSalesOrder"
 
-    html = frappe.get_print(doctype=doctype, name=docname, print_format=None, as_pdf=False, doc=doc, output=None, no_letterhead=0)
-    return html
+        html = frappe.get_print(doctype=doctype, name=docname, print_format=None, as_pdf=False, doc=doc, output=None, no_letterhead=0)
+        return html
 
-    response = Response()
-    response.mimetype = 'application/msword'
-    response.charset = 'utf-8'
-    response.filename = "{name}.doc".format(name=doc.title.replace(" ", "-").replace("/", "-"))
-    response.data = html
+        response = Response()
+        response.mimetype = 'application/msword'
+        response.charset = 'utf-8'
+        response.filename = "{name}.doc".format(name=doc.title.replace(" ", "-").replace("/", "-"))
+        response.data = html
+    except Exception as e:
+        return [str(e)]
     return response
