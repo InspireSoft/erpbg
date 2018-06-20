abb516b1ebe045fc36b07bc7d9f2a9f6afc0e930
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-20 09:59:33 +0300

    more debug data

--- erpbg/public/js/Quotation.js
+++ erpbg/public/js/Quotation.js
@@ -26,7 +26,9 @@ function check_for_communication_images(frm) {
 function notes_on_refresh() {
     cur_frm.doc.notes.forEach(function(note) {
         if(!note.cdn && note.iidx) {
-            cur_frm.doc.notes[note.idx].cdn = cur_frm.doc.item[note.iidx-1].name;
+            console.error(cur_frm.doc.items);
+            cur_frm.doc.notes[note.idx].cdn = cur_frm.doc.items[note.iidx-1].name;
+            cur_frm.doc.notes[note.idx].iidx = "";
             cur_frm.refresh_field('notes');
         }
     });
