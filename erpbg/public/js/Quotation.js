2b64cb1f1223fe780bb5b78610a2768d5fc568c2
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-20 08:59:50 +0300

    debug info

--- erpbg/public/js/Quotation.js
+++ erpbg/public/js/Quotation.js
@@ -25,6 +25,7 @@ function check_for_communication_images(frm) {
 
 frappe.ui.form.on('Quotation Item', {
     items_remove: function(doc,cdt,cdn) {
+        console.error(locals[cdt][cdn]);
         var nidx = -1;
         cur_frm.doc.notes.forEach(function(notes) {
             if(notes.iidx == locals[cdt][cdn].idx) {
