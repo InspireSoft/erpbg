dd847b4038a5a81af2963cae555f07f2d94abfd4
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-21 03:24:02 +0300

    code removal

--- erpbg/public/js/QuotationItem.js
+++ erpbg/public/js/QuotationItem.js
@@ -26,32 +26,6 @@ frappe.ui.form.on("Quotation Item", "item_code", function (frm, cdt, cdn) {
             }
         });
     }
-    if(!frm.doc.__islocal || frm.doc.__islocal == 0) {
-        frappe.call({
-            method: "frappe.client.get",
-            args: {
-                doctype: "Item",
-                filters: {
-                    item_code: locals[cdt][cdn].item_code
-                }
-            },
-            callback: function(r) {
-                if(r.message.image) {
-                    var skip = false;
-                    cur_frm.doc.quotation_attachment.forEach(function(qa) {
-                        if(qa.attachment.name == r.message.image.name) {
-                            skip = true;
-                        }
-                    });
-                    if(!skip) {
-                        var child = cur_frm.add_child("quotation_attachment");
-                        frappe.model.set_value(child.doctype, child.name, "attachment", r.message.image);
-                        cur_frm.refresh();
-                    }
-                }
-            }
-        });
-    }
 });
 
 frappe.ui.form.on("Quotation Item", "cdescription", function (frm, cdt, cdn) {
