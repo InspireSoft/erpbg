7dad4f62d34306f03e7780b0b4ca598a519fb0fa
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-27 12:49:02 +0300

    Quotation note bug fix

--- erpbg/public/js/QuotationNote.js
+++ erpbg/public/js/QuotationNote.js
@@ -1,7 +1,10 @@
 // Quotation Note
 
 frappe.ui.form.on("Quotation Note", "note", function (frm, cdt, cdn) {
-    console.error(frm);
-    frm.doc.note_view = stripHtml(frm.doc.note);
-    frm.refresh_field('note_view');
+    cur_frm.doc.notes.forEach(function(note2) {
+        if(note2.name == cdn) {
+            cur_frm.doc.notes[note2.idx-1].note_view = stripHtml(note2.note);
+        }
+    });
+    frm.refresh_field('notes');
 });
