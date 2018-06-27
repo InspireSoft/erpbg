0942ad87734099b32822e5ca3bc9512a4ce8675b
Simeon-Banov <simeon.banov@bulwork.com>
2018-06-27 12:35:45 +0300

    Quotation note debug info

--- erpbg/public/js/QuotationNote.js
+++ erpbg/public/js/QuotationNote.js
@@ -13,6 +13,7 @@ frappe.ui.form.on("Quotation Note", "onload_post_render", function (frm, cdt, cd
 
 
 frappe.ui.form.on("Quotation Note", "note", function (frm, cdt, cdn) {
+console.log(frm);
     frm.doc.note_view = jQuery(frm[cdt][cdn].note).text();
     frm.refresh_field('note_view');
 });
